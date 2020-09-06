a037f433f9cc129126b358e94fffb803e5b8240d
Max Shaian <championing@mail.ru>
2020-09-06 13:07:12 +0700

    Sitemap added

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(env('APP_URL'))->writeToFile(public_path() . '/sitemap.xml');
+
+        $this->info('The sitemap has been created successfully');
+    }
+}
