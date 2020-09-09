f8200f368a6eca0e61163fbb93cb7123d65e9624
Max Shaian <championing@mail.ru>
2020-09-09 22:26:55 +0700

    Instagram added

--- config/data.php
+++ config/data.php
@@ -8,5 +8,5 @@ return [
     'email' => 'asilva002@mail.ru',
     'email_2' => 'alikgrup@mail.ru',
     'address' => 'г. Красноярск, ул. Грунтовая, д. 19А, офис 28',
-    'instagram' => 'https://instagram.com',
+    'instagram' => 'https://www.instagram.com/alikgroup01/',
 ];
